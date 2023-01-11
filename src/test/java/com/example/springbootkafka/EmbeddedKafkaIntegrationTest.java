63f0ce50e85478c72102c11cccf07c5bfcc1fe7c
Sergey Savrukhin <savra.sv@yandex.ru>
2023-01-11 20:25:43 +0300

    Initial commit

---
+++ src/test/java/com/example/springbootkafka/EmbeddedKafkaIntegrationTest.java
@@ -0,0 +1,38 @@
+package com.example.springbootkafka;
+
+import com.example.KafkaConsumer;
+import com.example.KafkaProducer;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.kafka.test.context.EmbeddedKafka;
+import org.springframework.test.annotation.DirtiesContext;
+
+import java.util.concurrent.TimeUnit;
+
+@SpringBootTest
+@DirtiesContext
+@EmbeddedKafka(partitions = 1,
+        brokerProperties = { "listeners=PLAINTEXT://localhost:9092", "port=9092" })
+class EmbeddedKafkaIntegrationTest {
+
+    @Autowired
+    private KafkaConsumer consumer;
+
+    @Autowired
+    private KafkaProducer producer;
+
+    @Value("${test.topic}")
+    private String topic;
+
+    @Test
+    public void givenEmbeddedKafkaBroker_whenSendingtoSimpleProducer_thenMessageReceived()
+            throws Exception {
+        producer.send(topic, "Sending with own simple KafkaProducer");
+        consumer.getLatch().await(10000, TimeUnit.MILLISECONDS);
+
+        long count = consumer.getLatch().getCount();
+        String payload = consumer.getPayload();
+    }
+}
